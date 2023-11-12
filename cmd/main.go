7a07e95f31d946aaae16b9d22b2897966041a475
syordanov94 <syordanov@xmltravelgate.com>
2023-11-12 17:45:33 +0100

    minor changes

--- cmd/main.go
+++ cmd/main.go
@@ -16,7 +16,7 @@ var MigrationsFS embed.FS
 func main() {
 	// --- (1) ----
 	// Recover Migrator
-	migrator := migrator.NewMigrator(MigrationsFS, migrationsDir)
+	migrator := migrator.MustGetNewMigrator(MigrationsFS, migrationsDir)
 
 	// --- (2) ----
 	// Get the DB instance
--- internal/migrator/migrator.go
+++ internal/migrator/migrator.go
@@ -6,7 +6,6 @@ import (
 	"embed"
 	"errors"
 	"fmt"
-	"log"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
@@ -18,10 +17,10 @@ type Migrator struct {
 	srcDriver source.Driver
 }
 
-func NewMigrator(sqlFiles embed.FS, dirName string) *Migrator {
+func MustGetNewMigrator(sqlFiles embed.FS, dirName string) *Migrator {
 	d, err := iofs.New(sqlFiles, dirName)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	return &Migrator{
 		srcDriver: d,
@@ -31,12 +30,12 @@ func NewMigrator(sqlFiles embed.FS, dirName string) *Migrator {
 func (m *Migrator) ApplyMigrations(ctx context.Context, db *sql.DB) error {
 	driver, err := postgres.WithInstance(db, &postgres.Config{})
 	if err != nil {
-		return fmt.Errorf("unable to create psql instance: %v", err)
+		return fmt.Errorf("unable to create db instance: %v", err)
 	}
 
 	migrator, err := migrate.NewWithInstance("migration_embeded_sql_files", m.srcDriver, "psql_db", driver)
 	if err != nil {
-		return fmt.Errorf("unable to create psql migrate: %v", err)
+		return fmt.Errorf("unable to create migration: %v", err)
 	}
 
 	defer func() {
